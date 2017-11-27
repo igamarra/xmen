790de1e8bfa9aa734a3e3c4b93d6b8a3a16077c4
Ignacio Gamarra <ignacio.gamarra@almundo.com>
2017-11-26 22:06:25 -0300

    removing default tests

--- src/test/java/mutant/MutantControllerTests.java
+++ src/test/java/mutant/MutantControllerTests.java
@@ -16,6 +16,7 @@
 package mutant;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,20 +36,5 @@ public class MutantControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Test
-    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-
-        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, World!"));
-    }
-
-    @Test
-    public void paramGreetingShouldReturnTailoredMessage() throws Exception {
-
-        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
-    }
 
 }
